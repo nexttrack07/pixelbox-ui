b0daa2e8397b44be4bfb7b78bf0378cb12f3e529
faadhil hakim <faadhil1991@gmail.com>
2022-06-07 17:55:47 -0500

    image upload works

--- src/lib/editor.svelte
+++ src/lib/editor.svelte
@@ -10,7 +10,39 @@
     }
   });
 
-  $: console.log("authToken: ", $authToken);
+  let photo;
+
+  const handlePhotoUpload = async (e: any) => {
+    const image = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = (e) => {
+      photo = e.target.result;
+    };
+
+    const formData = new FormData();
+    formData.append("photo", image);
+
+    const options = {
+      method: "POST",
+      body: formData,
+    };
+
+    const res = await fetch("http://localhost:8000/api/upload-photo/", options);
+    const data = await res.json();
+
+    console.log("Data: ", data)
+  };
 </script>
 
 <h1>Hello from Editor</h1>
+<input type="file" accept=".jpg,.jpeg,.png" on:change={handlePhotoUpload} />
+
+{#if photo}
+  <img src={photo} alt="d" class="w-32 h-32" />
+{:else}
+  <img
+    src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+    alt=""
+  />
+{/if}
